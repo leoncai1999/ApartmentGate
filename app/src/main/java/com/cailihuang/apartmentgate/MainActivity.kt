0910e2af3d3b6ea115bef800c310df940d10a38f
= <leoncai197@gmail.com>
2019-11-11 15:44:33 -0600

    Added Map Fragment

--- app/src/main/java/com/cailihuang/apartmentgate/MainActivity.kt
+++ app/src/main/java/com/cailihuang/apartmentgate/MainActivity.kt
@@ -2,18 +2,21 @@ package com.cailihuang.apartmentgate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initMapFragment()
 
         navigationView.setOnNavigationItemSelectedListener {item ->
             when (item.itemId) {
                 R.id.navigation_search -> {
                     println("search pressed")
+                    initMapFragment()
                     true
                 }
                 R.id.navigation_popular -> {
@@ -33,6 +36,13 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    private fun initMapFragment() {
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_frame, MapFragment.newInstance())
+                .commit()
     }
 
 }
--- app/src/main/java/com/cailihuang/apartmentgate/MapFragment.kt
+++ app/src/main/java/com/cailihuang/apartmentgate/MapFragment.kt
@@ -1,4 +1,60 @@
 package com.cailihuang.apartmentgate
 
-class MapFragment {
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.common.GooglePlayServicesUtil
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_map.*
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import java.util.*
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    companion object {
+        fun newInstance(): MapFragment {
+            return MapFragment()
+        }
+    }
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_map, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFrag) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+        return rootView
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        // start map at center of San Francisco
+        val startLocation = LatLng(37.775124, -122.419163)
+        println("gets here")
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(startLocation, 12.0f))
+    }
+
 }
