a739fb4478361f006c8b797f0a451fb56d931755
Jason Lihuang <jasonlihuang@Jasons-MacBook-Pro.local>
2019-11-15 18:21:16 -0800

    basic create user account

--- app/src/main/java/com/cailihuang/apartmentgate/CreateAccountActivity.kt
+++ app/src/main/java/com/cailihuang/apartmentgate/CreateAccountActivity.kt
@@ -5,16 +5,55 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_create_account.*
 import kotlinx.android.synthetic.main.activity_welcome.*
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import android.widget.EditText
+
 
 class CreateAccountActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mDatabaseReference: DatabaseReference
+    private lateinit var mDatabase: FirebaseDatabase
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
 
-        createProfileButton.setOnClickListener {
+        initialize()
+
+        createProfileButton.setOnClickListener { createNewAccount() }
+
+    }
+
+    private fun initialize() {
+
+        auth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference!!.child("Users")
+
+    }
+
+    private fun createNewAccount() {
+        // TODO check if fields are empty
+
+        val email = newEmailET.text.toString()
+        val password = newPasswordET.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+            // do success things
+
+            Log.d("EMAILS IS --- " , email)
+
             val mainActivityIntent = Intent(this, MainActivity::class.java)
             startActivity(mainActivityIntent)
         }
     }
+
 }
