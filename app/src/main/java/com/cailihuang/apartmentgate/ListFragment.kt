8513f7b766d1de759a858abd4c0447128716be0c
Jason Lihuang <jasonlihuang@Jasons-MacBook-Pro.local>
2019-11-29 23:22:44 -0800

    set up some of skeleton of sort filter

--- app/src/main/java/com/cailihuang/apartmentgate/ListFragment.kt
+++ app/src/main/java/com/cailihuang/apartmentgate/ListFragment.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -22,6 +23,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_list.*
+import android.widget.AdapterView
 
 
 class ListFragment: Fragment() {
@@ -42,6 +45,38 @@ class ListFragment: Fragment() {
         rv.layoutManager = LinearLayoutManager(context)
     }
 
+    private fun initializeLayoutElems() {
+        ArrayAdapter.createFromResource(
+            context!!,
+            R.array.sort_by_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            sortSpinner.adapter = adapter
+        }
+
+        sortSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // do nothing
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> println("sort by da da da")
+                    1 -> println("etc")
+                }
+
+            }
+
+        }
+
+        filtersButton.setOnClickListener {
+            // launch new filters fragment probably
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -56,11 +91,14 @@ class ListFragment: Fragment() {
         initAdapter(root)
         viewModel.populateFavorites()
 
+
+
         val listings = mutableListOf<ApartmentListing>()
         val ref = FirebaseDatabase.getInstance().getReference("listings").child("TZAVBG6NoTmSCv1tFdhe").child("apartment")
 
         ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                initializeLayoutElems()
 
                 for (productSnapshot in dataSnapshot.children) {
                     val listing = productSnapshot.getValue(ApartmentListing::class.java)
